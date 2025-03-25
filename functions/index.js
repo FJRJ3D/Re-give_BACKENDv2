b10f56fdd8a7e2b71cb16e0466f0701f08ee8b7e
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2025-03-25 19:07:13 +0100

    Added CRUD methods for Product

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,36 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { onRequest } = require("firebase-functions/v2/https");
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+initializeApp();
+const db = getFirestore();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+exports.createProduct = onRequest({ region: "us-central1" }, async (request, response) => {
+    const docRef = await db.collection("products").add(request.body);
+    const newDoc = await docRef.get();
+    const createdItem = { id: newDoc.id, ...newDoc.data() };
+    response.json( createdItem );
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.getAllProducts = onRequest({ region: "us-central1" }, async (request, response) => {
+    const snapshot = await db.collection("products").get();
+    const products = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    response.json(products);
+});
+
+exports.updateProduct = onRequest({ region: "us-central1" }, async (request, response) => {
+    const id = request.url.replace(/^\/+|\/+$/g, '');
+    const data = request.body;
+    const productRef = db.collection("products").doc(id);
+    const productDoc = await productRef.get();
+    await productRef.update(data);
+    const updatedProduct = { id: productDoc.id, ...productDoc.data(), ...data };
+    response.json(updatedProduct);
+});
+
+exports.deleteProduct = onRequest({ region: "us-central1" }, async (request, response) => {
+    const id = request.url.replace(/^\/+|\/+$/g, '');
+    const productRef = db.collection("products").doc(id);
+    await productRef.delete();
+    response.json(`Product with id ${id} deleted successfully`);
+});
